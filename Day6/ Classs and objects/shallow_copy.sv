8cb7b3ea9f6a8dbbba3887598ed6f2f79fa571a4
Kiran589 <101051183+Kiran589@users.noreply.github.com>
2024-08-01 15:15:05 +0530

    Create shallow_copy.sv

---
+++ Day6/ Classs and objects/shallow_copy.sv
@@ -0,0 +1,34 @@
+class Mirafra;
+  string c;
+  int d;
+
+  function new();
+    c = "Teams";
+    d = 8;
+  endfunction:new
+
+  function void display();
+    $display("\t c=%0s,\t d=%0d",c,d);
+  endfunction:display
+
+endclass:Mirafra
+
+module shallow;
+  Mirafra p1;
+  Mirafra p2;
+
+  initial begin:BEGIN_I
+    p1 =new();
+    $display("contents of p1");
+    p1.display();
+    p2 =new p1;
+    $display("contents of p2");
+    p2.display();
+    p2.c="place";
+    $display("diplay contents of p1");
+    p1.display();
+    $display("diplay contents of p2");
+    p2.display();
+  end:BEGIN_I
+
+endmodule
